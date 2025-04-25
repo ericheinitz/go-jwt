5b2c5a3bb3d1502dbb387446a8e320eaf1a68eb6
ericheinitz <eeyh@bsmartlab.com>
2025-04-25 13:35:32 -0300

    Add database connection and user model initialization

---
+++ initializers/connectToDb.go
@@ -0,0 +1,20 @@
+package initializers
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDb() {
+	var err error
+	dsn := os.Getenv("DB")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("fallo la conexión a la base de datos")
+	}
+}
