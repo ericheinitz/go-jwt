af3536f6dba423258c8841347bb46830e2fcd6f3
ericheinitz <eeyh@bsmartlab.com>
2025-04-25 12:56:12 -0300

    go-jwt

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/ericheinitz/go-jwt/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run()
+}
